51d0744995cb2a8e41d81b276a354c7c2c584ac6
毕殿卿 <bdq123@outlook.com>
2017-11-21 21:23:24 +0800

    添加项目文件。

---
+++ DotNetCoreSendEmail/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace DotNetCoreSendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient client = new SmtpClient("smtp.163.com");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("bidianqing123", "your password");
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("bidianqing123@163.com");
+            mailMessage.To.Add("bidianqing@qq.com");
+            mailMessage.Body = "body";
+            mailMessage.Subject = "subject";
+            client.Send(mailMessage);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
