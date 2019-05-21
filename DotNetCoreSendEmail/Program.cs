d2396b0dc6cfbe5d9a63aa392d90d9d8a2012949
毕殿卿 <bdq123@outlook.com>
2019-05-21 15:32:49 +0800

    *

--- DotNetCoreSendEmail/Program.cs
+++ DotNetCoreSendEmail/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using System.Net;
-using System.Net.Mail;
+//using System.Net.Mail;
 
 namespace DotNetCoreSendEmail
 {
@@ -8,9 +10,10 @@ namespace DotNetCoreSendEmail
     {
         static void Main(string[] args)
         {
+            /*
             SmtpClient client = new SmtpClient("smtp.163.com");
             client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("bidianqing123", "password");
+            client.Credentials = new NetworkCredential("bidianqing123", "19910916bdq");
 
             MailMessage mailMessage = new MailMessage();
             mailMessage.From = new MailAddress("bidianqing123@163.com");
@@ -18,6 +21,31 @@ namespace DotNetCoreSendEmail
             mailMessage.Body = "body";
             mailMessage.Subject = "subject";
             client.Send(mailMessage);
+            //*/
+
+            // Use MailKit
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("毕殿卿","bidianqing123@163.com"));
+            message.To.Add(new MailboxAddress("bidianqing","bidianqing@qq.com"));
+            message.Subject = "Test";
+            message.Body = new TextPart("plain")
+            {
+                Text = "测试"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.163.com", 25, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("bidianqing123", "19910916bdq");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
